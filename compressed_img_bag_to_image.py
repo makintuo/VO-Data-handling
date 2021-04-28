08a7bf73543a5b6c7f0161cf669babdd2380421d
makintuo <64179973+makintuo@users.noreply.github.com>
2021-04-28 14:53:47 +0900

    Update compressed_img_bag_to_image.py

--- compressed_img_bag_to_image.py
+++ compressed_img_bag_to_image.py
@@ -3,7 +3,7 @@ import cv2
 import sys
 import os
 import numpy as np
-from Cryptodome.Cipher import AES
+
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 import datetime
@@ -12,10 +12,10 @@ from multiprocessing import Process
 # python3 compressed_img_bag_to_image.py PATH_TO_BAGFILE PATH_TO_OUTPUT_DIR NAME_OF_TARGET_TOPIC OUTPUT_SUFFIC[png|jpg]
 # python3 compressed_img_bag_to_image.py ./2020-07-11-15-51-36.bag ./right /camera/right/image_color/compressed png
 
-
+f = open("/home/kamijolab/Downloads/times.txt", "w")
 
 def unix2datetime(unix_time):
-    date_time = datetime.datetime.fromtimestamp(unix_time) + datetime.timedelta(hours=9)
+    date_time = datetime.datetime.fromtimestamp(unix_time) # + datetime.timedelta(hours=9)
     return date_time.strftime("%Y-%m-%dT%H.%M.%S.%f")
 
 # arguments
@@ -29,6 +29,9 @@ if output_suffix != "jpg" and output_suffix != "png":
 
 # load bag file
 bag = rosbag.Bag(bag_path).read_messages()
+time = rosbag.Bag(bag_path).get_start_time()
+
+print(time)
 print("bag file loaded!")
 
 def write_msg_image(msg, t):
@@ -36,8 +39,9 @@ def write_msg_image(msg, t):
     np_array = np.frombuffer(msg.data, np.uint8)
     image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
     image = cv2.resize(image, (819, 600))
-    date_time = unix2datetime(t.to_sec())
-    output_path = os.path.join(output_dir, date_time+"."+output_suffix)
+    #date_time = unix2datetime(t.to_sec())
+    #output_path = os.path.join(output_dir, "{0:.6e}".format(float(t.to_sec())-float(time)) +"."+output_suffix)
+    output_path = os.path.join(output_dir, str("{:.6f}".format(float(t.to_sec())-float(time))) +"."+output_suffix)
 
     # output the image
     cv2.imwrite(output_path, image)
@@ -49,10 +53,12 @@ def multi_write_msg_image(buff_list):
 process_list = []
 buff = []
 
+
 # loop in each message
 for topic, msg, t in bag:
     if topic==target_topic:
         buff.append((msg, t))
+        f.write(str("{0:.6e}\n".format(float(t.to_sec())-float(time))))
         if len(buff) == 16:
             process = Process(
 	            target=multi_write_msg_image,
@@ -63,3 +69,5 @@ for topic, msg, t in bag:
 
 for process in process_list:
     process.join()
+    
+f.close()
